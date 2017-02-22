e24dc4a89724ee35bb81b3c1c170b394284a01e3
Alejandro Gómez <agp901@gmail.com>
2017-02-22 17:47:47 +0100

    Usando *ngFor
    creando un array de personajes, donde se muestra de manera automatizada en una lista gracias a la propiedad *ngFor, enumerandola gracias a ; let i = 'index'

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -8,6 +8,7 @@ export class BodyComponent implements OnInit {
   private texto:string = 'Este texto pertenece a una propiedad la clase Body Component';
   private autor:string = 'Alejandro!';
   private mostrar:boolean = true;
+  private personajes:string[] = ['Spiderman','Venom','Dr. Octopus'];
 
 
   constructor() { }
